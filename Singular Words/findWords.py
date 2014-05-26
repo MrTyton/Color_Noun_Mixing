4e303ad3d7d8d8090ab878311aa00dd48738fb93
Joshua Gang <joshua.gang@gmail.com>
2014-05-26 16:23:51 -0400

    Changed to individual items instead of grouped by color, allowing for more precise filtering of wanted noun + color labels. Further parsing required, as there are some duplicates and some erronious labels that slipped through

--- Singular Words/findWords.py
+++ Singular Words/findWords.py
@@ -10,22 +10,13 @@ name_groups = []
 for x in names:
     for y in names:
         if x is not y and x in y and "dark" not in y and "light" not in y and "bright" not in y and "pale" not in y:
-            try:
-                name_groups[x].append(y)
-            except KeyError:
-                name_groups[x] = [y]
-
-name_groups = [(x, name_groups[x]) for x in name_groups]
-
-name_groups.sort(key=lambda x: len(x[1]))
+            name_groups.append([x, y])
 
 print len(name_groups)
 
 groups_copy = name_groups[:]
 for x in groups_copy:
-    print("Do you wish to keep the following:\n\n%s\n" % (x[0]))
-    for y in x[1]:
-        print("\t%s\n" % y)
+    print("Do you wish to keep the following:\n\n%s\n\t%s\n" % (x[0], x[1]))
     ans = raw_input("Blank for yes, n for no: ")
     if ans != "":
         name_groups.remove(x)
@@ -35,9 +26,7 @@ for x in groups_copy:
 fp = open("wordlist.txt", "w")
 
 for x in name_groups:
-    fp.write("%s\n" % (x[0]))
-    for y in x[1]:
-        fp.write("\t%s\n" % y)
+    fp.write("%s\n\t%s\n" % (x[0], x[1]))
         
 fp.close()
 

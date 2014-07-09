afcfe8eb70c91175211e78ff70de19db569b0734
MrTyton <joshua.gang@gmail.com>
2014-07-09 15:31:30 -0400

    rewrote createTrees a bit, split it up with testTrees, doing work on that

--- new-trees/createTrees.py
+++ new-trees/createTrees.py
@@ -1,5 +1,4 @@
 from sklearn.ensemble import RandomForestRegressor
-from dataManip import *
 import pickle
 
 with open("training_data.pkl", "r") as fp:
@@ -7,33 +6,25 @@ with open("training_data.pkl", "r") as fp:
 
 print "Creating Forest"
 
-forest_regressor = RandomForestRegressor()
-forest_regressor.fit([x[0] for x in training_data], [x[1][1] for x in training_data])
 
+branch = 1
+
+if branch == 0:
+    forest_regressor = RandomForestRegressor()
+    forest_regressor.fit([x[0] for x in training_data], [x[1][1] for x in training_data])
+    with open("forest_regressor.pkl", "w") as fp:
+        pickle.dump(forest_regressor, fp)
+else:
+    forest_regressor_hue = RandomForestRegressor()
+    forest_regressor_saturation = RandomForestRegressor()
+    forest_regressor_value = RandomForestRegressor()
+    forest_regressor_hue.fit([x[0][0:14] + x[0][38:52] for x in training_data], [x[1][1][0:14] for x in training_data])
+    forest_regressor_saturation.fit([x[0][14:26] + x[0][52:64] for x in training_data], [x[1][1][14:26] for x in training_data])
+    forest_regressor_value.fit([x[0][26:38] + x[0][64:76] for x in training_data], [x[1][1][26:38] for x in training_data])
+    with open("forest_regressor.pkl", "w") as fp:
+        pickle.dump((forest_regressor_hue, forest_regressor_saturation, forest_regressor_value), fp)
 #print forest_regressor.feature_importances_ #have to use this to see which features it finds important, could be trying to predict too many things at once
 
-with open("testing_data.pkl", "r") as fp:
-    testing_data = pickle.load(fp)
 
-results = forest_regressor.predict([x[0] for x in testing_data])
-
-r2_tests = []
-
-total = len(results)
-
-fp = open("raw_output.txt", "w")
-
-for i, (x, y) in enumerate(zip(results, [x[1][0] for x in testing_data])):
-    print "Testing %d out of %d"  % (i+1, total)
-    predicted = createDistribution(y, x)
-    test_result = r2test(y, LUX.getColor(y), predicted)
-    r2_tests.append(test_result)
-    
-    fp.write("%s\n%f\n%s\n%s\n\n----\n" % (y, test_result, LUX.getColor(y).printStats(), predicted.printStats()))
-
-fp.close()    
-     
-with open("results.pkl", "w") as fp:
-    pickle.dump(r2_tests, fp)
     
     
---
+++ new-trees/testTrees.py
@@ -0,0 +1,44 @@
+from sklearn.ensemble import RandomForestRegressor
+from dataManip import *
+import pickle
+
+with open("testing_data.pkl", "r") as fp:
+    testing_data = pickle.load(fp)
+
+if branch == 0:
+    results = forest_regressor.predict([x[0] for x in testing_data])
+else:
+    hues = forest_regressor_hue.predict([x[0][0:14] + x[0][38:52] for x in testing_data])
+    saturation = forest_regressor_saturation.predict([x[0][14:26] + x[0][52:64] for x in testing_data])
+    value = forest_regressor_value.predict([x[0][26:38] + x[0][64:76] for x in testing_data])
+    def appending(x, y, z):
+        ans = []
+        ans.extend(x)
+        ans.extend(y)
+        ans.extend(z)
+        return ans
+    results = [appending(x, y, z) for x, y, z in zip(hues, saturation, value)]
+
+r2_tests = []
+
+total = len(results)
+
+fp = open("raw_output.txt", "w")
+
+for i, (x, y) in enumerate(zip(results, [x[1][0] for x in testing_data])):
+    print "Testing %d out of %d"  % (i+1, total)
+    predicted = createDistribution(y, x)
+    test_result = r2test(y, LUX.getColor(y), predicted)
+    r2_tests.append(test_result)
+    
+    fp.write("%s\n%f\n%s\n%s\n\n----\n" % (y, test_result, LUX.getColor(y).printStats(), predicted.printStats()))
+
+    if i == 0 or i == 1 or i == 5 or i == 27:
+        plotData(y, LUX.getColor(y), predicted, axis="H")
+        plotData(y, LUX.getColor(y), predicted, axis="S")
+        plotData(y, LUX.getColor(y), predicted, axis="V")
+
+fp.close()    
+     
+with open("results.pkl", "w") as fp:
+    pickle.dump(r2_tests, fp)

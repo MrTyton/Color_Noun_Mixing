744172e3a00c598eeb65014324923e302074e020
MrTyton <joshua.gang@gmail.com>
2014-07-14 14:27:27 -0400

    with the exception of purple blue, which I think bugged out, this seemed to do a much better job, having implemented the k-fold 72->1 forest trees.

--- new-trees/createTrees.py
+++ new-trees/createTrees.py
@@ -8,25 +8,13 @@ with open("%s/training_data.pkl" % (directory), "r") as fp:
 
 print "Creating Forest"
 
-
-branch = 1
-
-if branch == 0:
+target_data = [x[1][1][2:] for x in training_data]
+forests = []
+for i in range(len(target_data[0])):
     forest_regressor = RandomForestRegressor()
-    forest_regressor.fit([x[0] for x in training_data], [x[1][1] for x in training_data])
-    with open("%s/forest_regressor.pkl" % (directory), "w") as fp:
-        pickle.dump(forest_regressor, fp)
-else:
-    forest_regressor_hue = RandomForestRegressor()
-    forest_regressor_saturation = RandomForestRegressor()
-    forest_regressor_value = RandomForestRegressor()
-    forest_regressor_hue.fit([x[0][2:14] + x[0][40:52] for x in training_data], [x[1][1][2:14] for x in training_data])
-    forest_regressor_saturation.fit([x[0][14:26] + x[0][52:64] for x in training_data], [x[1][1][14:26] for x in training_data])
-    forest_regressor_value.fit([x[0][26:38] + x[0][64:76] for x in training_data], [x[1][1][26:38] for x in training_data])
-    with open("%s/forest_regressor.pkl" % (directory), "w") as fp:
-        pickle.dump((forest_regressor_hue, forest_regressor_saturation, forest_regressor_value), fp)
-#print forest_regressor.feature_importances_ #have to use this to see which features it finds important, could be trying to predict too many things at once
+    forest_regressor.fit([x[0][2:38] + x[0][40:] for x in training_data], [x[i] for x in target_data])
+    forests.append(forest_regressor)
 
-
-    
+with open("%s/forest_regressor.pkl" % (directory), "w") as fp:
+    pickle.dump(forests, fp) 
     
--- new-trees/testTrees.py
+++ new-trees/testTrees.py
@@ -4,30 +4,20 @@ import pickle
 
 directory = "./Fake Data"
 
-branch = 2
-
 with open("%s/forest_regressor.pkl" % (directory), "r") as fp:
-    if branch == 0:
-        forest_regressor = pickle.load(fp)
-    else:
-        forest_regressor_hue, forest_regressor_saturation, forest_regressor_value = pickle.load(fp)
+    forests = pickle.load(fp)
 
 with open("%s/testing_data.pkl" % (directory), "r") as fp:
     testing_data = pickle.load(fp)
 
-if branch == 0:
-    results = forest_regressor.predict([x[0] for x in testing_data])
-else:
-    hues = forest_regressor_hue.predict([x[0][2:14] + x[0][40:52] for x in testing_data])
-    saturation = forest_regressor_saturation.predict([x[0][14:26] + x[0][52:64] for x in testing_data])
-    value = forest_regressor_value.predict([x[0][26:38] + x[0][64:76] for x in testing_data])
-    def appending(x, y, z):
-        ans = []
-        ans.extend(x)
-        ans.extend(y)
-        ans.extend(z)
-        return ans
-    results = [appending(x, y, z) for x, y, z in zip(hues, saturation, value)]
+initial_results = []
+
+for current in forests:
+    initial_results.append(current.predict([x[0][2:38] + x[0][40:] for x in testing_data]))
+
+results = []
+for i in range(len(initial_results[0])):
+    results.append([x[i] for x in initial_results])
 
 r2_tests = []
 
@@ -38,22 +28,19 @@ fp = open("%s/raw_output.txt" % (directory), "w")
 for i, (x, y) in enumerate(zip(results, [x[1][0] for x in testing_data])):
     print "Testing %d out of %d"  % (i+1, total)
     predicted = createDistribution(y, x)
-    if branch != 2:
-        actual = LUX.getColor(y)
-    else:
-        with open("Fake Data/new_wordlist.pkl", "r") as fp2:
-            words = pickle.load(fp2)
-        actual = createDistribution(y, words[y])
+    with open("%s/new_wordlist.pkl" % (directory), "r") as fp2:
+        words = pickle.load(fp2)
+    actual = createDistribution(y, words[y])
     test_result = r2test(y, actual, predicted)
     r2_tests.append(test_result)
     
     fp.write("%s\n%f\n%s\n%s\n\n----\n" % (y, test_result, actual.printStats(), predicted.printStats()))
 
-    #if i == 0 or i == 1 or i == 5 or i == 27:
-    #    print test_result
-    #    plotData(y, LUX.getColor(y), predicted, axis="H")
-    #    plotData(y, LUX.getColor(y), predicted, axis="S")
-    #    plotData(y, LUX.getColor(y), predicted, axis="V")
+#    if i == 0 or i == 5 or i == 27:
+#        print test_result
+#        plotData(y, LUX.getColor(y), predicted, axis="H")
+#        plotData(y, LUX.getColor(y), predicted, axis="S")
+#        plotData(y, LUX.getColor(y), predicted, axis="V")
 
 fp.close()    
      

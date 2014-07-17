a45c6a7c3105af3ccdad0a4d59abe3bbb6a76a4c
MrTyton <joshua.gang@gmail.com>
2014-07-17 15:35:47 -0400

    Wrote the KL Divergence test, but I don't think that all of my P and Qs are summing to 1, so it's possible for me to get negative values, which should not be possible. Thought that I had fixed that with the AUC normalization, but it could be slightly off. Maybe should figure out how to do the availability normalization again?

--- dataManip.py
+++ dataManip.py
@@ -1,4 +1,4 @@
-from math import log
+from math import log, fabs
 import lux
 LUX = lux.LUX('lux.xml')
 import matplotlib.pyplot as plt
@@ -170,7 +170,27 @@ def r2test(name, real_distribution, generated_distribution, HSV=None):
     SS_res = sum([(x - y) ** 2 for x, y in zip(real_phi, generated_phi)])
     r2 = 1 - (SS_res / SS_tot)
     return r2
-    
+
+def klDivergance(name, real_distribution, generated_distribution, HSV=None):
+    data = getData("Data", name)
+    if HSV is None:
+        real_phi = [real_distribution(x) for x in data]
+        generated_phi = [generated_distribution(x) for x in data]
+    else:
+        axis = {"H": 0, "S": 1, "V": 2}
+        axis = axis[HSV]
+        real_phi = [real_distribution.dim_models[axis].phi(x) for x in [y[axis] for y in data]]
+        generated_phi = [generated_distribution.dim_models[axis].phi(x) for x in [y[axis] for y in data]] 
+    mathy = []
+    for p, q in zip(real_phi, generated_phi):
+        if fabs(p) < 1e-7 and fabs(q) < 1e-7:
+            continue
+        logging = log((p/q),)
+        if fabs(logging) < 1e-7 and fabs(p) < 1e-7:
+            continue
+        mathy.append(logging * p)
+    Dkl = sum(mathy)
+    return Dkl
     
     
     
--- tests.py
+++ tests.py
@@ -1,23 +1,21 @@
-import lux
-print "Importing LUX"
-LUX = lux.LUX('lux.xml')
 import matplotlib.pyplot as plt
+print "Importing Data Manip"
 import dataManip
+LUX = dataManip.LUX
 
 
 print "Grabbing Colors"
 green_blue = LUX.getColor("green-blue")
 blue_green = LUX.getColor("blue-green")
 
-for x in [green_blue, blue_green]:
-    x.dim_models[0].auc(360)
-    x.dim_models[1].auc(100)
-    x.dim_models[2].auc(100)
+green_blue([0, 0, 0])
+blue_green([0, 0, 0])
 print "Testing"
 
-print dataManip.r2test("green-blue", green_blue, green_blue)
-print dataManip.r2test("green-blue", green_blue, blue_green)
-print dataManip.r2test("blue-green", blue_green, green_blue)
+print dataManip.klDivergance("green-blue", green_blue, green_blue)
+print dataManip.klDivergance("blue-green", blue_green, blue_green)
+print dataManip.klDivergance("green-blue", green_blue, blue_green)
+print dataManip.klDivergance("blue-green", blue_green, green_blue)
 
     
 #dataManip.plotData("green-blue", green_blue, blue_green, "H")

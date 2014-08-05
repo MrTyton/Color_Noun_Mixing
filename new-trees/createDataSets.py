22097fdf7bce4df3f3f5fcf0a2ea6572f9be4bd8
MrTyton <joshua.gang@gmail.com>
2014-08-05 03:36:02 -0400

    Doing both Broadness + Skew, sorted with respect to broadness, gave an increase over doing either of those individually, on average. There are, as always, some cases that are worse.

--- new-trees/createDataSets.py
+++ new-trees/createDataSets.py
@@ -12,22 +12,25 @@ manipulated_wordlist = []
 
 for i, (composed, first, second) in enumerate(composed_wordlist):
     print "Working on %d out of %d, %s and %s." % (i+1, len(composed_wordlist), first, second)
-    with open("../skew_test.pkl", "r") as fp:
+    with open("../broadness_test.pkl", "r") as fp:
         broadness = pickle.load(fp)
-    
+    with open("../skew_test.pkl", "r") as fp:
+        skew = pickle.load(fp)
     current = []
     start = 2
     first_vector = convertToVector(first)
     second_vector = convertToVector(second)
-    for x, y in zip(broadness[first], broadness[second]):
-        first_comparison = x#log(x)
-        first_vector[1] = first_comparison
-        second_comparison = y#log(y)
-        second_vector[1] = second_comparison
+    for x, y, z, a in zip(broadness[first], broadness[second], skew[first], skew[second]):
+        first_comparison = log(x)
+        first_vector[0] = first_comparison
+        first_vector[1] = z
+        second_comparison = log(y)
+        second_vector[0] = second_comparison
+        second_vector[1] = a
         if first_comparison > second_comparison:
-            current.append((first_vector[1:2] + first_vector[start:start+12] + second_vector[1:2] + second_vector[start:start+12]))
+            current.append((first_vector[0:2] + first_vector[start:start+12] + second_vector[0:2] + second_vector[start:start+12]))
         else:
-            current.append((second_vector[1:2] + second_vector[start:start+12] + first_vector[1:2] + first_vector[start:start+12]))
+            current.append((second_vector[0:2] + second_vector[start:start+12] + first_vector[0:2] + first_vector[start:start+12]))
         start += 12
     composed_vector = convertToVector(composed)
     manipulated_wordlist.append(((current[0], current[1], current[2]), composed, (composed_vector[2:14], composed_vector[14:26], composed_vector[26:38])))       

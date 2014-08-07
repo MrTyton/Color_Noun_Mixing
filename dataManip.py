Rewrote auc to actually just use integrals - fixed the KL-distance test, using sum of integrals, which should be equivalent to the triple integral.

dataManip.py
@@ -1,5 +1,5 @@
 from math import log, fabs
-from scipy.integrate import tplquad
+from scipy.integrate import tplquad, quad
 import lux
 LUX = lux.LUX('lux.xml')
 import matplotlib.pyplot as plt
@@ -175,7 +175,11 @@ def r2test(name, real_distribution, generated_distribution, HSV=None):
 def klDivergence(name, real_distribution, generated_distribution, HSV=None):
     real_distribution([0,0,0])
     generated_distribution([0,0,0])
-    return tplquad(lambda x, y, z: log(real_distribution([x, y, z]) / generated_distribution([x, y, z])) * real_distribution([x, y, z]), 0, 360, lambda x : 0, lambda x: 100, lambda x, y: 0, lambda x, y: 100)
+    first_dim = quad(lambda x : log(real_distribution.dim_models[0].phi(x) / generated_distribution.dim_models[0].phi(x)) * real_distribution.dim_models[0].phi(x), 0, 360)
+    second_dim = quad(lambda x : log(real_distribution.dim_models[1].phi(x) / generated_distribution.dim_models[1].phi(x)) * real_distribution.dim_models[1].phi(x), 0, 100)
+    third_dim = quad(lambda x : log(real_distribution.dim_models[2].phi(x) / generated_distribution.dim_models[2].phi(x)) * real_distribution.dim_models[2].phi(x), 0, 100)
+    return first_dim[0] + second_dim[0] + third_dim[0]
+    #return tplquad(lambda z, y, x :  log(real_distribution([x, y, z]) / generated_distribution([x, y, z])) * real_distribution([x, y, z]), 0, 360, lambda x : 0, lambda x: 100, lambda x, y: 0, lambda x, y: 100)
     
     
     

lux.py
@@ -9,6 +9,8 @@ There is an accompanying CSV file.  This script opens it and offers basic functi
 from scipy.stats import gamma as gam_dist
 from math import sin, cos, atan2, pi, fabs, log
 import xml.etree.ElementTree as ET
+from scipy.integrate import quad
+
 class LUX:
     def __init__(self, filename):
         """
@@ -154,15 +156,7 @@ class single_dim:
     def auc(self, maximum):
         maximum = float(maximum)
         if self.auc_val is None:
-            h = maximum/(2 * 999.)
-            summation = 0
-            for i in self.frange(0, maximum, maximum / 1000.):
-                if i != 0 and i != maximum:
-                    summation += 2 * self.phi(i)
-                else:
-                    summation += self.phi(i)
-            ans = h * summation
-            self.auc_val = ans #/ maximum
+            self.auc_val = quad(lambda x : self.phi(x), 0, maximum)[0]
         return self.auc_val
 
     def printStats(self):
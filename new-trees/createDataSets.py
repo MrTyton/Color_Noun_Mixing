d0a7146632ac98eb2ad613fb60d27f9e8825b8e7
MrTyton <joshua.gang@gmail.com>
2014-09-30 10:13:09 -0400

    Revert "Tried to use a combination of skew + non-skew, using an intermedant classifier to determine which forest to use. Didn't go so well, I think because it might not have trained the forests on enough data."
    This reverts commit c3930af1b96d9690879ebde21c4fc4718b362b29.

--- new-trees/createDataSets.py
+++ new-trees/createDataSets.py
@@ -1,7 +1,7 @@
 
 from dataManip import *
 import pickle
-destination = "./Both Classifier"
+destination = "./Split + Broadness - Availability - Adjust"
 
 with open("composed_wordlist.pkl", "r") as fp:
     composed_wordlist = pickle.load(fp)
@@ -28,25 +28,21 @@ for i, (composed, first, second) in enumerate(composed_wordlist):
         second_vector[0] = second_comparison
         second_vector[1] = a
         if first_comparison > second_comparison:
-            current.append((first_vector[0:2] + first_vector[start:start+12] + second_vector[0:2] + second_vector[start:start+12]))
+            current.append((first_vector[0:1] + first_vector[start:start+12] + second_vector[0:1] + second_vector[start:start+12]))
         else:
-            current.append((second_vector[0:2] + second_vector[start:start+12] + first_vector[0:2] + first_vector[start:start+12]))
+            current.append((second_vector[0:1] + second_vector[start:start+12] + first_vector[0:1] + first_vector[start:start+12]))
         start += 12
     composed_vector = convertToVector(composed)
     manipulated_wordlist.append(((current[0], current[1], current[2]), composed, (composed_vector[2:14], composed_vector[14:26], composed_vector[26:38])))       
                                 
 composed_wordlist = manipulated_wordlist #now composed_wordlist is x, y, z, with x being the less broad, y being the more broad, and z being the composed
 
-#diff = len(composed_wordlist) - (len(composed_wordlist) / 3)
-diff = len(composed_wordlist) / 3
+diff = len(composed_wordlist) - (len(composed_wordlist) / 3)
 
 training_data = composed_wordlist[:diff]
-training_data_2 = composed_wordlist[diff: 2 * diff]
-testing_data = composed_wordlist[diff * 2:]
+testing_data = composed_wordlist[diff:]
 
-with open("%s/training_data_skew.pkl" % (destination), "w") as fp:
+with open("%s/training_data.pkl" % (destination), "w") as fp:
     pickle.dump(training_data,fp)
-with open("%s/training_data_skew_2.pkl" % (destination), "w") as fp:
-    pickle.dump(training_data_2, fp)
-with open("%s/testing_data_skew.pkl" % (destination), "w") as fp:
+with open("%s/testing_data.pkl" % (destination), "w") as fp:
     pickle.dump(testing_data, fp)
--- new-trees/createTrees.py
+++ new-trees/createTrees.py
@@ -1,9 +1,9 @@
 from sklearn.ensemble import RandomForestRegressor
 import pickle
 
-directory = "./Both Classifier"
+directory = "./Split + Broadness - Availability - Adjust"
 
-with open("%s/training_data_skew.pkl" % (directory), "r") as fp:
+with open("%s/training_data.pkl" % (directory), "r") as fp:
     training_data = pickle.load(fp)
 
 print "Creating Forest"
@@ -15,6 +15,6 @@ for i in range(0, 3):
     current_forest.fit([x[0][i] for x in training_data], [x[2][i] for x in training_data])
     forests.append(current_forest)
 
-with open("%s/forest_regressor_skew.pkl" % (directory), "w") as fp:
+with open("%s/forest_regressor.pkl" % (directory), "w") as fp:
     pickle.dump(forests, fp) 
     
